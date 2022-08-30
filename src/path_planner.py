d4a44a65f2fafbff20123f380e13062c4a9c5a4f
alexandredorc <ddorcival@gmail.com>
2022-08-30 22:42:43 +1000

    smooth path

--- src/path_planner.py
+++ src/path_planner.py
@@ -629,22 +629,15 @@ class PathSmoother():
         # Loop until the smoothing converges
         # In each iteration, update every waypoint except the first and last waypoint
 
-        #########################
-        ## YOUR CODE GOES HERE ##
-        #########################
+        for j in range(100):
+            for i in range(1,len(path)-1):
+                path[i].x+=alpha*path_nodes[i].x+beta*path[i-1].x+beta*path[i+1].x-(alpha+2*beta)*path[i].x
+                path[i].y+=alpha*path_nodes[i].y+beta*path[i-1].y+beta*path[i+1].y-(alpha+2*beta)*path[i].y
 
+            
+        blocked = is_occluded(self.graph_.map_.image_, [x1, y1], [x2, y2])
 
-
-
-
-
-
-
-
-        
-        
-
-        return path_smooth
+        return path
 
 
 
